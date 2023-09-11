a02b00982cf8a697827f9043b577ec40ca964a4e
MariyaBes <bessarabenko.m.i20@gmail.com>
2023-09-12 00:14:24 +0300

    Лабораторная работа №1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(
+    {name: "Mariya", age: 20}
+  )
+
+  const [liked, setLiked] = useState(false);
+
+  const ageCountInc = () => {
+    setUser({...user, age: user.age + 1});
+  }
+
+  const ageCountDec = () => {
+    setUser({ ...user, age: user.age - 1 });
+  }
+
+  function changeName(e) {
+    if (e.key === 'Enter') {
+      setUser({ ...user, name: e.target.value, age: 20 });
+    }
+  }
+
+  const chageLike = () => {
+    setLiked(!liked
+    )
+  }
+
+  console.log(user)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Задание 2</h2>
+      <input
+        type="text"
+        placeholder="Введите имя"
+        value={user.name}
+        onChange={e=>setUser(e.target.value)}
+        onKeyDown={changeName}
+      />
+
+      <h1>Your name: {user.name} </h1>
+
+      <h1>Age: {user.age} </h1>
+
+      <button onClick={ageCountInc}>Increase</button>
+      <button onClick={ageCountDec}>Decrease</button>
+
+      <h2>Задание 3</h2>
+
+      <label>
+        <input type="checkbox" class='check' onClick={chageLike}></input>
+        Мне нравится это
+        </label>
+      <p>{liked ? "Тебе нравится это" : "Тебе не нравится это"}</p>
     </div>
   );
 }
